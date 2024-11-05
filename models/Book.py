d62ecf7022387a352be2c32a94b88714c8064441
isaak kbr <chrisisaak22@gmail.com>
2024-11-05 19:57:11 +0100

    Implemented more accurate book availability check by considering ongoing loans within a 15-day window

--- models/Book.py
+++ models/Book.py
@@ -1,4 +1,5 @@
 from odoo import models,fields
+from datetime import timedelta
 
 class Book(models.Model):
     _name = 'books'
@@ -9,3 +10,14 @@ class Book(models.Model):
     pages = fields.Integer()
     quantity = fields.Integer()
     author_name = fields.Char()
+    
+    def _book_available(self):
+        for book in self:
+            borrowed_count = self.env['borrowed_books'].search_count([
+                ('book_id', '=', book.id),
+                ('is_returned', '=', False),
+                ('borrow_date', '>=', fields.Date.today()),
+                ('return_date', '<=', fields.Date.today() + timedelta(days=15))
+            ])
+            
+            return max(0, book.quantity + 1 - borrowed_count) != 0
