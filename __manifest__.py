0cde88dd138ac39b3824319fa8d845593b44ca93
isaak kbr <chrisisaak22@gmail.com>
2024-11-05 12:09:31 +0100

    Update Configuration with Basic Security Access

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name' : 'Library Management',
+    'author' : 'Isaak Kbr',
+    'category' : '',
+    'version' : '17.0.0.1.0',
+    'depends' : [
+        'base',
+    ],
+    'data' : [
+        'security/ir.model.access.csv',
+        'views/index.xml',
+        'views/students.xml',
+        'views/books.xml',
+    ],
+    'application' : True
+}
